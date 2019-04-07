43cd0c8f9cad47498109c0f22b172082030f7ddd
enyo10 <enyo10@yahoo.fr>
2019-04-07 10:10:15 +0200

    Authentication wiht firebase, googlemap creation

---
+++ app/src/main/java/ch/enyo/openclassrooms/go4lunch/base/BaseActivity.java
@@ -0,0 +1,68 @@
+package ch.enyo.openclassrooms.go4lunch.base;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.ButterKnife;
+import ch.enyo.openclassrooms.go4lunch.R;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    //--------------------
+    // LIFE CYCLE
+    // --------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.getActivityLayout());
+        FirebaseApp.initializeApp(this);
+
+        ButterKnife.bind(this);
+    }
+
+    // --------------------
+    // UI
+    // --------------------
+    protected void configureToolbar(){
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    // --------------------
+    // ERROR HANDLER
+    // --------------------
+
+    protected OnFailureListener onFailureListener(){
+        return new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), getString(R.string.error_unknown_error), Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    @Nullable
+    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+
+    public abstract int getActivityLayout();
+
+
+
+
+}
