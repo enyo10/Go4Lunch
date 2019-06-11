7750be8925811301b6b1ae2c725fb07939cc9499
enyo10 <enyo10@yahoo.fr>
2019-06-11 19:26:46 +0200

    Key name change in gradle.properties

---
+++ app/src/main/java/ch/enyo/openclassrooms/go4lunch/controllers/activities/SearchableActivity.java
@@ -0,0 +1,14 @@
+package ch.enyo.openclassrooms.go4lunch.controllers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
